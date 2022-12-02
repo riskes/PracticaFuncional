5535f3c613621c60d4dc62040b1a59283bd7dec8
Arnau Rísquez Guiteras <arisquezg@gmail.com>
2022-12-02 22:29:22 +0100

    optimització codi

--- src/main/scala/Simil.scala
+++ src/main/scala/Simil.scala
@@ -1,4 +1,6 @@
-import scala.collection.MapView
+import java.util
+import scala.collection.immutable.HashSet
+import scala.collection.{MapView, mutable}
 
 object Simil extends App {
 import scala.io.Source
@@ -7,53 +9,35 @@ import scala.io.Source
   def main(): List[(String, Int)] = {
     //ubitext = scala.io.StdIn.readLine();
     val text: String = Source.fromFile("C:\\Users\\arisq\\Downloads\\Practica Funcional + Objectes (1a. part)-20221024\\pg11.txt").mkString
-    ngrama(3,text)
-
+    nonstopfreq(text)
   }
 
   def splitWords(llibre: String): Array[String] = {
-    val llistaparaules: String = llibre.replaceAll("[\\W\\r\\n\\t\\d_]", " ")
-    val llistaparaules1: String = llistaparaules.replaceAll("( +)", " ")
-    return llistaparaules1.toLowerCase().split(" ").filter(_.nonEmpty)
+    llibre.replaceAll("[\\W\\r\\n\\t\\d_]+", " ").toLowerCase().split(" ").filter(_.nonEmpty)
   }
   def freq(llibre: String): List[(String, Int)] ={
-
-    var ocurrencies: List[(String,Int)]= List()
-    val llistaparaules: Array[String] = splitWords(llibre)
-
-    for(x <- 0 to llistaparaules.length-1){
-      ocurrencies = (llistaparaules(x), 1) :: ocurrencies
-    }
-    val prova: List[(String, Int)] = ocurrencies.groupBy(_._1).map{ _._2.reduce({ (a,b) => (a._1, a._2+b._2)})}.toList.sortBy(_._2).reverse
-    prova
+      splitWords(llibre).map(w => (w, 1)).groupBy(_._1).map{ _._2.reduce({ (a,b) => (a._1, a._2+b._2)})}.toList.sortBy(_._2).reverse
   }
 
   def nonstopfreq(llibre: String) : List[(String, Int)] = {
 
-    //ubitext = scala.io.StdIn.readLine();
     val text: String = Source.fromFile("C:\\Users\\arisq\\Downloads\\Practica Funcional + Objectes (1a. part)-20221024\\english-stop.txt").mkString
-    val textSplit: Array[String] = splitWords(text)
-    var ocurrencies: List[(String, Int)] = List()
-    val llistaparaules: Array[String] = splitWords(llibre)
+    var textSplit : Set[String] = new HashSet[String]
+    textSplit = splitWords(text).toSet
 
-    for (x <- 0 to llistaparaules.length - 1) {
-      if (textSplit.contains(llistaparaules(x))) {
-      } else {
-        ocurrencies = (llistaparaules(x), 1) :: ocurrencies
-      }
-    }
-      val prova: List[(String, Int)] = ocurrencies.groupBy(_._1).map {
-        _._2.reduce({ (a, b) => (a._1, a._2 + b._2) })
-      }.toList.sortBy(_._2).reverse
+    //Split de les 11
+    splitWords(llibre).filter(w => !textSplit.contains(w)).map(w => (w,1)).groupBy(_._1).map {
+    _._2.reduce({ (a, b) => (a._1, a._2 + b._2) })
+    }.toList.sortBy(_._2).reverse
 
-      println("Num de Paraules: " + ocurrencies.length + "  Diferents: " + prova.length)
+      /*println("Num de Paraules: " + ocurrencies.length + "  Diferents: " + prova.length)
       println("Paraules             ocurrencies             frequencia")
       println("---------------------------------------------------------")
       for (x <- 0 to 9) {
         val frequencia = ((prova(x)._2*100).toDouble/ocurrencies.length)
         println(prova(x)._1 + "      " + prova(x)._2 + "      " + f"$frequencia%1.2f")
-      }
-      prova
+      }*/
+
 
   }
   def paraulafreqfreq(Llibre: String) {
@@ -72,18 +56,15 @@ import scala.io.Source
 
 
   def ngrama(n: Int, llibre: String): List[(String, Int)] ={
-    print("dintre funcio")
-    var ocurrencies: List[(String, Int)] = List()
+    var ocurrencies: List[(String, Int)] = freq(llibre)
 
-    val llistaparaules: List[String] = splitWords(llibre).sliding(3).toList.map((x) => x.mkString(" "))
-    for (x <- 0 to llistaparaules.length - 1) {
+    val llistaparaules: List[String] = splitWords(llibre).sliding(n).toList.map(x => x.mkString(" "))
+    for (x <- 0 until llistaparaules.length - 1) {
       ocurrencies = (llistaparaules(x), 1) :: ocurrencies
     }
-    val prova: List[(String, Int)] = ocurrencies.groupBy(_._1).map {
+    ocurrencies.groupBy(_._1).map {
       _._2.reduce({ (a, b) => (a._1, a._2 + b._2) })
     }.toList.sortBy(_._2).reverse
-    print("ey")
-    prova
   }
 main()
 };
